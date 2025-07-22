0ef079deca3ef4887708a0399dc26d79880ec5f4
Student <Student@Train11-5-PC115.control.ad.sit.kmutt.ac.th>
2025-07-22 15:25:16 +0700

    Add class2

---
+++ class2.py
@@ -0,0 +1,135 @@
+# age = 18
+# print(age)
+
+# variable
+# num = 5
+# print(type(num))
+
+# num2 = 1.1
+# print(type(num2))
+
+# boolean = True 
+# print(type(boolean))
+# True False
+
+# name = "Da 005"
+# print(type(name))
+# print(name)
+
+# age = "18"
+# print(type(age))
+
+# age2 = int(age)
+# print(type(age2))
+
+# num1 = 5.5
+# print(type(num1))
+
+# num2 = int(num1)
+# print(type(num2))
+# print(num2)
+
+# num1 = 5
+# print(type(num1))
+
+# num2 = float(num1)
+# print(type(num2))
+# print(num2)
+# float ทศนิยม
+
+# age = 18.9
+# print(type(age))
+
+# age2 = str(age)
+# print(type(age2))
+
+# tf = "True"
+# print(type(tf))
+
+# tf2 = bool(tf)
+# print(type(tf2))
+
+# tf = True
+# print(type(tf))
+
+# tf2 = str(tf)
+# print(type(tf2))
+
+# operation 
+
+# sum = 1 + 2
+# print(sum)
+# #str + - / int float ไม่ได้ แต่ * ได้
+
+# name = "Da" + "kkd"
+# print(name)
+
+# sum = 1 + True
+# print(sum)
+# True 1 False 0
+
+# sum = 5.3 - 3
+# print(sum)
+
+# sum = 9 -True
+# print(sum)
+
+# sum = 5.5*2
+# print(sum)
+
+# sum = "D" * 9
+# print(sum)
+
+# sum = 5/2
+# print(sum)
+
+# sum = 2 / True
+# print(sum)
+# False ใช้ไม่ได้
+
+# sum = 5%2
+# print(sum)
+# % ให้แค่เศษ
+
+# sum = 2**3
+# print(sum)
+# ยกกำลังตัวตั้งตัวแรกเป็นเลข
+
+# sum = 5 == 5
+# print(sum)
+
+# sum = "Da" == "Da"
+# print(sum)
+
+# sum = "9" == 9
+# print(sum)
+# เช็ค Datatype
+
+# sum = "str" > "4"
+# print(sum)
+# str นับความยาวตัวอักษร
+
+#Input Output 
+# age = input("Enter your age : ")
+# print("Your age is" , age)
+#input รับค่า Str เสมอ
+# print(type(age))
+
+# age = int(input("Enter your age : "))
+# print("Your age is" , age)
+# print(type(age))
+
+# name = input("What is your name : ")
+# print("Your name is" , name)
+# print(type(name))
+
+# age = int(input("Enter your age : "))
+
+#Input Output
+# if age > 18:
+#     print("Your age more than 18")
+# elif age == 18:
+#     print("Your age is 18")
+# else:
+#     print("Your age lower than 18")
+
